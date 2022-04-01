1e180ea9b551203f6fe5bd186e3685a3a35ff2c9
zachrey <zz__0123@163.com>
2022-04-01 16:54:58 +0800

    feat(color): 修改颜色

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -7,9 +7,9 @@ if not status_ok then
   return
 end
 
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "light"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+-- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
