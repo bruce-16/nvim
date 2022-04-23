7e1846e68c0f820030c58e452ba3480a3e24d512
zachrey <zz__0123@163.com>
2022-04-23 23:41:11 +0800

    feat(plugin): 增加 plugin config

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ require("plugins")
 require("colorscheme")
 -- 插件配置
 require("plugin-config.nvim-tree")
--- require("plugin-config.bufferline")
+require("plugin-config.bufferline")
 require("plugin-config.lualine")
 require("plugin-config.telescope")
 require("plugin-config.dashboard")
