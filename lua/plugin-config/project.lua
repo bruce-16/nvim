dcb5919078f039185dc48a0ebf229d4214079504
zachrey <zz__0123@163.com>
2022-08-24 11:35:04 +0800

    fix(plugin): 修改 project 插件的配置，设置为手动模式

--- lua/plugin-config/project.lua
+++ lua/plugin-config/project.lua
@@ -9,6 +9,8 @@ vim.g.nvim_tree_respect_buf_cwd = 1
 
 project.setup({
   detection_methods = { "pattern" },
+  -- 设置为手动模式，禁止 project 插件自动修改 root_dir
+  manual_mode = true,
   patterns = { ".git", "Makefile", "package.json" },
 })
 
