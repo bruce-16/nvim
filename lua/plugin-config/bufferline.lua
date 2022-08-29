5d4ab5c4035465b737371887656410062b65ac4c
zachrey <zz__0123@163.com>
2022-08-29 16:13:49 +0800

    modify(plugin): bufferline 取消关闭 icon

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -8,11 +8,13 @@ end
 -- https://github.com/akinsho/bufferline.nvim#configuration
 bufferline.setup({
   options = {
+    show_buffer_close_icons = false,
     show_close_icon = false,
-    close_icon = '',
+    -- close_icon = '',
     -- 关闭 Tab 的命令，这里使用 moll/vim-bbye 的 :Bdelete 命令
     close_command = "Bdelete! %d",
     right_mouse_command = "Bdelete! %d",
+    separator_style = 'slant',
     -- 侧边栏配置
     -- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
     offsets = {
