c7e7cd0bbe939a221641cce9c43221b8a7bdba32
zachrey <zz__0123@163.com>
2022-09-01 15:25:37 +0800

    modify(autocmd): 修改部分自动 cmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -9,17 +9,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Remove statusline and tabline when in Alpha
--- vim.api.nvim_create_autocmd({ "User" }, {
---   pattern = { "AlphaReady" },
---   callback = function()
---     vim.cmd [[
---       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
---       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
---     ]]
---   end,
--- })
-
 -- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown" },
@@ -29,30 +18,48 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
+  clear = true,
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- nvim-tree 打开文件后自动关闭
+autocmd("BufEnter", {
+  nested = true,
+  group = myAutoGroup,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd("quit")
+    end
+  end,
+})
+
+-- 用o换行不要延续注释
+autocmd("BufEnter", {
+  group = myAutoGroup,
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions
+      - "o" -- O and o, don't continue comments
+      + "r" -- But do continue when pressing enter.
+  end,
+})
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = myAutoGroup,
+  pattern = "*",
+})
 
 -- Fixes Autocomment
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
+autocmd("BufWinEnter", {
+  group = myAutoGroup,
+  pattern = "*",
+  command = "set formatoptions-=cro",
 })
 
--- Highlight Yanked Text
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
-  end,
-})
-
--- yank 时高亮展示
--- pcall(
---   vim.cmd,
---   [[
---   augroup highlight_yank
---   autocmd!
---   au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=150}
---   augroup END
---   ]]
--- )
-
+-- vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
